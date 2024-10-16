c483b5840a85bf4f9f52508c3529f2154bfbf16a
Rishit Makadia <rishitmakadia@gmail.com>
2024-10-16 10:32:05 +0530

    Add files via upload

---
+++ queue.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#define MAX 2
+
+int queue[MAX];
+int front=-1, rear=-1;
+
+void insertion(ele)
+{
+    if (rear == MAX-1)
+    printf("Queue Overflow");
+    else 
+    {
+        queue[++rear]=ele;
+    }
+}
+void deletion()
+{
+    int del;
+    if (rear == -1)
+    printf("Queue is Empty");
+    else
+        del=queue[++front];
+}
+void display()
+{
+    int i = front;
+    if (rear == -1)
+    printf("Queue is Empty");
+    else 
+    {
+        printf("Queue = ");
+        while (i != rear)
+        {
+            printf("\t%d", queue[++i]);
+        }
+    }
+}
+
+int main()
+{
+    int opt, no;
+    while (1)
+    {
+        printf("\n1. Insertion\t2. Deletion\t3. Display\t4. Exit\nEnter Option:");
+        scanf("%d", &opt);
+        switch (opt)
+        {
+            case 1:
+            {
+                printf("Enter Element: ");
+                scanf("%d", &no);
+                insertion(no);
+                break;
+            }
+            case 2:
+            {
+                deletion();
+                break;
+            }
+            case 3:
+            {
+                display();
+                break;
+            }
+            case 4:
+                return 0;
+            default:
+                printf("Incorrect Input\nTry Again");
+        }
+    }
+}
